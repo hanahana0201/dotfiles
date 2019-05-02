a5cc77a1c29fbfe28bcefb1287927801a4e1acc4
Masatoshi HANAI <hanahana0201@gmail.com>
2019-05-03 00:33:56 +0900

    tmuxの追加

--- setup.sh
+++ setup.sh
@@ -7,6 +7,13 @@ do
   ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
+# tmux
+DOT_FILES=(.tmux.conf)
+for file in ${DOT_FILES[@]}
+do
+  ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
 # nvim
 DOT_FILES=(init.vim)
 for file in ${DOT_FILES[@]}
